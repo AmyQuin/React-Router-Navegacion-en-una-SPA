6cc08c4365321376d4c9a1354ea4e94e7a8ea174
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-07-10 13:59:50 -0600

    02. Creando componentes
    Lo que aprendimos en esta aula:
    
    -Hemos creado un componente 404 para cuando el usuario intente acceder alguna página que no existe.
    -Vimos la diferencia entre una SPA (Single Page Application) y un MPA (Multi Pages Application).
    -Montamos una navbar utilizando React Router para simular una MPA (Multi Pages Application).

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
+import Page404 from './pages/Page404'; //Pagina 404
+import Header from './Components/Header';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //Utilizazmos react router dom
 
 function App() {
 
  
-
-  
+//CREANDO RUTAS
+//Route path='*' es una ruta aleartoria, si no existe se muestra el mensaje del h1
 //Cuando enviamos informacion con props no es necesario poner <></>
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>     
-        <Route path='/sobre' element={<Sobre/>}/>                    
+        <Route path='/sobre' element={<Sobre/>}/>    
+        <Route path='*' element={<Page404/>}/>                
       </Routes>
     </Router>
   );
